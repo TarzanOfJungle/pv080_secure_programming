012bb4e3749845a70fc4f11d335bae2a837f0224
Ondrej Nemec <xnemec7@fi.muni.cz>
2024-04-17 09:04:55 +0200

    fix: code injection

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -60,6 +60,9 @@ if __name__ == '__main__':
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
+        while (urlib_version != "2" and urlib_version != "3") {
+            urlib_version = input("Choose version of urllib (2 or 3): ")
+        }
         fetch_website(urlib_version, url="https://www.google.com")
     elif choice == "3":
         load_yaml(input("File name: "))
